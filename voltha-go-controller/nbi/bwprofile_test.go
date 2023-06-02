c6805d6bcb8013f85dbee0f83e7405d476664493
vinokuma <vinod.kumar@radisys.com>
2023-06-02 18:08:01 +0530

    Adding UTs part-1
    Change-Id: I7fdb7751f536d1a616cf2a81b7ef16747d71f692

---
+++ voltha-go-controller/nbi/bwprofile_test.go
@@ -0,0 +1,103 @@
+/*
+* Copyright 2023-present Open Networking Foundation
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package nbi
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestProfileHandle_GetProfile(t *testing.T) {
+	req, err := http.NewRequest("GET", "/get_profile/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	vars := map[string]string{
+		"id": "upstream_bw_profile_gpon",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	rr := httptest.NewRecorder()
+
+	type args struct {
+		cntx context.Context
+		w    http.ResponseWriter
+		r    *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "GetProfile",
+			args: args{
+				cntx: context.Background(),
+				w:    rr,
+				r:    req,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mh := &ProfileHandle{}
+			mh.GetProfile(tt.args.cntx, tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestProfileHandle_DelProfile(t *testing.T) {
+	var jsonStr = []byte(`{"id":"test_id"}`)
+
+	req, err := http.NewRequest("DELETE", "/del_profile/", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+
+	type args struct {
+		cntx context.Context
+		w    http.ResponseWriter
+		r    *http.Request
+	}
+	tests := []struct {
+		name string
+		mh   *ProfileHandle
+		args args
+	}{
+		{
+			name: "DelProfile",
+			args: args{
+				cntx: context.Background(),
+				w:    rr,
+				r:    req,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mh := &ProfileHandle{}
+			mh.DelProfile(tt.args.cntx, tt.args.w, tt.args.r)
+		})
+	}
+}
