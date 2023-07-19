3776c4cd44dfa73c25772695a568fee51e501f26
Hitesh Chhabra <hitesh.chhabra@radisys.com>
2023-07-19 13:15:16 +0530

    Logging improvement for VGC-NB
    Change-Id: I3ee79fbbe6e5ea4d1caafd117576401bc5071f9e

--- voltha-go-controller/nbi/subscriber.go
+++ voltha-go-controller/nbi/subscriber.go
@@ -103,18 +103,19 @@ func (sh *SubscriberHandle) AddSubscriberInfo(cntx context.Context, w http.Respo
 	// Get the payload to process the request
 	d := new(bytes.Buffer)
 	if _, err := d.ReadFrom(r.Body); err != nil {
-		logger.Warnw(ctx, "Error reading buffer", log.Fields{"Reason": err.Error()})
+		logger.Errorw(ctx, "Error reading buffer", log.Fields{"Reason": err.Error()})
+		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	}
 
 	// Unmarshal the request into service configuration structure
 	req := &SubscriberDeviceInfo{}
 	if err := json.Unmarshal(d.Bytes(), req); err != nil {
-		logger.Warnw(ctx, "Unmarshal Failed", log.Fields{"Reason": err.Error()})
+		logger.Errorw(ctx, "Failed to Unmarshal Adding Subscriber", log.Fields{"req": req, "Reason": err.Error()})
 		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	}
-	logger.Debugw(ctx, "Received-northbound-add-service-request", log.Fields{"req": req})
+	logger.Infow(ctx, "Received-northbound-add-service-request", log.Fields{"req": req})
 
 	//vsCfgList := getVoltServiceFromSrvInfo(req)
 
@@ -125,10 +126,10 @@ func addAllService(cntx context.Context, srvInfo *SubscriberDeviceInfo) {
 	//vsCfgList := getVoltServiceFromSrvInfo(srvInfo)
 	va := app.GetApplication()
 	if len(srvInfo.UniTagList) == 0 {
-		logger.Debugw(ctx, "Received OLT configuration", log.Fields{"req": srvInfo})
+		logger.Infow(ctx, "Received OLT configuration", log.Fields{"req": srvInfo})
 		err := va.AddDeviceConfig(cntx, srvInfo.ID, srvInfo.HardwareIdentifier, srvInfo.NasID, srvInfo.IPAddress, srvInfo.UplinkPort, srvInfo.NniDhcpTrapVid)
 		if err != nil {
-			logger.Warnw(ctx, "Device config addition failed :", log.Fields{"Reason": err.Error()})
+			logger.Warnw(ctx, "Device config addition failed :", log.Fields{"req": srvInfo, "Reason": err.Error()})
 		}
 		return
 	}
@@ -155,6 +156,8 @@ func addAllService(cntx context.Context, srvInfo *SubscriberDeviceInfo) {
 		vs.DsMeterProfile = uniTagInfo.DownstreamBandwidthProfile
 		vs.IgmpEnabled = uniTagInfo.IsIgmpRequired
 		vs.ServiceType = uniTagInfo.ServiceName
+
+		logger.Debugw(ctx, "", log.Fields{"ServiceName": vs.Name})
 
 		if uniTagInfo.ServiceName == app.DpuMgmtTraffic ||
 			uniTagInfo.ServiceName == app.DpuAncpTraffic ||
@@ -194,6 +197,8 @@ func addAllService(cntx context.Context, srvInfo *SubscriberDeviceInfo) {
 		vnetName = vnetName + strconv.FormatUint(uint64(vs.CVlan), 10) + "-"
 		vnetName = vnetName + strconv.FormatUint(uint64(vs.UniVlan), 10)
 
+		logger.Debugw(ctx, "", log.Fields{"VnetName": vnetName})
+
 		vnetcfg := app.VnetConfig{
 			Name:              vnetName,
 			SVlan:             vs.SVlan,
@@ -232,7 +237,7 @@ func addAllService(cntx context.Context, srvInfo *SubscriberDeviceInfo) {
 			logger.Errorw(ctx, "AddVnet Failed", log.Fields{"VnetName": vnetName, "Error": err})
 		}
 		if err := app.GetApplication().AddService(cntx, vs, nil); err != nil {
-			logger.Errorw(ctx, "AddService Failed", log.Fields{"Service": vs.Name, "Error": err})
+			logger.Errorw(ctx, "AddService Failed", log.Fields{"Service": vs.Name, "Error": err.Error()})
 		}
 	}
 }
@@ -242,11 +247,10 @@ func (sh *SubscriberHandle) DelSubscriberInfo(cntx context.Context, w http.Respo
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	logger.Debugw(ctx, "Received-northbound-del-service-request", log.Fields{"req": id})
-
 	d := new(bytes.Buffer)
 	if _, err := d.ReadFrom(r.Body); err != nil {
-		logger.Warnw(ctx, "Error reading buffer", log.Fields{"Reason": err.Error()})
+		logger.Errorw(ctx, "Error reading buffer", log.Fields{"req id": id, "Reason": err.Error()})
+		w.WriteHeader(http.StatusConflict)
 		return
 	}
 
@@ -271,7 +275,7 @@ func (sh *SubscriberHandle) DelSubscriberInfo(cntx context.Context, w http.Respo
 		}
 	}
 
-	logger.Warnw(ctx, "northbound-del-service-req", log.Fields{"ServiceName": id})
+	logger.Infow(ctx, "Received northbound-del-service-req", log.Fields{"ServiceName": id})
 	err := app.GetApplication().DelServiceWithPrefix(cntx, id)
 	if err != nil {
 		logger.Warnw(ctx, "northbound-del-service-req failed, Subscriber not exist", log.Fields{"ServiceName": id})
